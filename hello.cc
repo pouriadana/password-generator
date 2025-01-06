f2708c7fc85afa15a0f99c352be384bb195ad68b
Pouria Dana <pouriadana73@gmail.com>
2025-01-06 21:21:14 +0330

    Redo erasure of pass_len variable

--- hello.cc
+++ hello.cc
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
     std::string password{};
     std::default_random_engine engine{rd()};
     std::uniform_int_distribution<int> distrib(0,viable_chars.size()-1);
-    
+    int pass_len{12};
     for (int i = 0; i < pass_len; i=i+3) {
         std::string t1{viable_chars[distrib(engine)]};
         std::string t2{viable_chars_capital[distrib(engine)]};

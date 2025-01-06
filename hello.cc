8e9f61d9822c029286f51a15ce29d061cb773f14
Pouria Dana <pouriadana73@gmail.com>
2025-01-06 16:46:07 +0330

    Update: Include small & capital letters

--- hello.cc
+++ hello.cc
@@ -5,12 +5,14 @@
 int main()
 {
     std::string viable_chars {"abcdefghijklmnopqrstuvwxyz"};
+    std::string viable_chars_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     std::random_device rd;
     std::default_random_engine engine{rd()};
     std::uniform_int_distribution<int> distrib(0,viable_chars.size()-1);
     const int pass_len{12};
-    for (int i = 0; i < pass_len; ++i) {
+    for (int i = 0; i < pass_len; i=i+2) {
         std::cout << viable_chars[distrib(engine)];
+        std::cout << viable_chars_capital[distrib(engine)];
     }
     std::cout << '\n';
 }

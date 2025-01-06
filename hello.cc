0adf5891ef2a90b3adcbf0e5ba97089c722caaae
Pouria Dana <pouriadana73@gmail.com>
2025-01-06 21:15:31 +0330

    Add non-alphabetic characters to the password creation process

--- hello.cc
+++ hello.cc
@@ -6,15 +6,17 @@ int main()
 {
     const std::string viable_chars {"abcdefghijklmnopqrstuvwxyz"};
     const std::string viable_chars_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    const std::string non_alphabetic_chars {"0123456789!?<>(){}@#$%^&*"};
     std::random_device rd;
     std::string password{};
     std::default_random_engine engine{rd()};
     std::uniform_int_distribution<int> distrib(0,viable_chars.size()-1);
     const int pass_len{12};
-    for (int i = 0; i < pass_len; i=i+2) {
+    for (int i = 0; i < pass_len; i=i+3) {
         std::string t1{viable_chars[distrib(engine)]};
         std::string t2{viable_chars_capital[distrib(engine)]};
-        password += t1 + t2;
+        std::string t3{non_alphabetic_chars[distrib(engine)]};
+        password += t1 + t2 + t3;
     }
     std::cout << '\n';
     std::cout << password << '\n';

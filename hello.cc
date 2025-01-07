363f8dc8b540b41244aecc74062676c9dc435617
Pouria Dana <pouriadana73@gmail.com>
2025-01-07 13:20:25 +0330

    Add started code to integrate birthdate into the password

--- hello.cc
+++ hello.cc
@@ -69,4 +69,10 @@ int main(int argc, char *argv[])
     }
     std::cout << '\n';
     std::cout << password << '\n';
+    /* Integrate birthdate into the password*/
+    int birthdate{static_cast<int>(atof(argv[2]))};
+    int disected_birthdate[2] {0,0};
+    disected_birthdate[0] = birthdate % 100;
+    birthdate /= 100;
+    disected_birthdate[1] = birthdate % 100;
 } 

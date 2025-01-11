239d9eea238973a8166475d72f1f4e01fd7dc1c3
Pouria Dana <pouriadana73@gmail.com>
2025-01-11 20:58:17 +0330

    add ability to specify length with specifier -l

--- hello.cc
+++ hello.cc
@@ -161,23 +161,24 @@ int main(int argc, char *argv[])
             }
         }
     }
-    // std::cout << "Length: " << length_val
-    //           << "\nyear: " << byear_val
-    //           << "\ncolor: " << favcolor_val
-    //           << "\nnumber: " << favnumber_val
-    //           << "\n";
+    std::cout << "Length: " << length_val
+              << "\nyear: " << byear_val
+              << "\ncolor: " << favcolor_val
+              << "\nnumber: " << favnumber_val
+              << "\n";
     const std::string alphabet {"abcdefghijklmnopqrstuvwxyz"};                          // length == 26
     const std::string alphabet_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};                  // length == 26
     const std::string numsymbol {"0123456789!?<>(){}@#$%^&*"};                          // length == 25
     std::string password{};
-    int pass_len{0};
-    if (argc == 1) {
+    int pass_len{length_val};
+    if (argc == 1) {                                                                    // in case no argument is given, give a default password length
         pass_len = MIN_PASS_LEN;
+        passlen_flag = true;
     }
-    else if (argc >= 2 && isdigit(argv[1])) {
-        pass_len = atof(argv[1]);
-    }
-    else {
+    // else if (argc >= 2 && isdigit(argv[1])) {
+    //     pass_len = atof(argv[1]);
+    // }
+    else if (!(passlen_flag || byear_flag || favcolor_flagg || favnumber_flag)) {
         std::cout << "Illegal input\n";
         std::exit(EXIT_FAILURE);
     }
@@ -186,13 +187,15 @@ int main(int argc, char *argv[])
     Rand_int rfor_numsymbol(0, numsymbol.size()-1);                                     // create a generator for the number/symbol data size
     Rand_int rfor_candidate(0, 1000);
     char candidates[3] {0,0,0};                                                         // hold, in each iteration, 3 different chars of different type: small, capital & num/sym
-    for (int i = 0; i < pass_len; ++i) {
+    if (passlen_flag == true) {
+        for (int i = 0; i < pass_len; ++i) {
         candidates[0] = alphabet[rfor_alphabet()];
         candidates[1] = alphabet_capital[rfor_alphabet()];
         candidates[2] = numsymbol[rfor_numsymbol()];
         int rsubscript{rfor_candidate()%3};                                             // select one char with a distinct type from the created candidate chars
         std::string chosen_char{candidates[rsubscript]};
         password += chosen_char;
+        }
     }
     // std::cout << '\n';                                                               // DEBUG
     // std::cout << password << '\n';                                                   // DEBUG
